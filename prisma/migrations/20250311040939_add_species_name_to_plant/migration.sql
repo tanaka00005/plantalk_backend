a938faa0d35f133d4eebd70f57c6c72c8f4d0428
tanaka00005 <x23054xx@aitech.ac.jp>
2025-03-30 01:39:52 +0900

    デプロイできた

--- prisma/migrations/20250311040939_add_species_name_to_plant/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `speciesName` to the `Plant` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Plant" ADD COLUMN     "speciesName" TEXT NOT NULL;
